83ee7cbb9c8ec505a3b1688a2dc376307df92993
jasonwang3150 <jasonwang636490@gmail.com>
2016-10-26 23:09:28 -0400

    JPA create

--- com.JPA.demo/src/main/java/com/JPA/pojo/Student.java
+++ com.JPA.demo/src/main/java/com/JPA/pojo/Student.java
@@ -1,9 +1,32 @@
 package com.JPA.pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="StudentList")
 public class Student {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
 	private Integer id;
 	private String name;
 	private String address;
+	
+	public Student(Integer id, String name, String address){
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
 	
 	public Integer getId() {
 		return id;
@@ -22,5 +45,10 @@ public class Student {
 	}
 	public void setAddress(String address) {
 		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+	   return "Student [id=" + id + ", name=" + name + ", address=" + address + "]";
 	}
 }
